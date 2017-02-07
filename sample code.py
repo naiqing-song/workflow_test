c24791b7238d3bdf8a2ea4274ae72280f5c528ee
naiqing song <qingchuyulan@gmail.com>
2017-02-06 23:29:53 -0800

    2nd feature

--- sample code.py
+++ sample code.py
@@ -5,6 +5,9 @@ def first_func():
 def first_feature():
     print('first_feature')
 
+def second_feature():
+    print('first_feature')
+
 def first_func_by_second_user():
     print(first_func_by_second_user)
 
