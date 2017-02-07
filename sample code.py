b7278a2d61b49053a3ecba6f88af7a859b72da33
naiqing song <qingchuyulan@gmail.com>
2017-02-06 23:34:46 -0800

    2nd feature amend

--- sample code.py
+++ sample code.py
@@ -6,7 +6,7 @@ def first_feature():
     print('first_feature')
 
 def second_feature():
-    print('first_feature')
+    print('second_feature')
 
 def first_func_by_second_user():
     print(first_func_by_second_user)
