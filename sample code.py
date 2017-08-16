f09375dd6b7de4b969c60822727234ae856ad028
naiqing song <naiqing.song@scryanalytics.com>
2017-08-16 13:51:56 -0700

    squashed workflow

---
+++ sample code.py
@@ -0,0 +1,38 @@
+def first_func():
+    print('first_func')
+
+
+def first_feature():
+    print('first_feature')
+
+def second_feature():
+    print('second_feature')
+
+def first_func_by_second_user():
+    print(first_func_by_second_user)
+
+def second_func_by_second_user():
+    print(second_func_by_second_user)
+
+def modify_by_master():
+    print modify_by_master
+
+def modify_by_feature():
+    print modify_by_feature
+
+def modify_by_master2():
+    print modify_by_master
+    
+def merge_commit_1
+
+def merge_commit_2
+
+squash_merge1
+
+squash_merge2
+
+rebase1
+
+rebase2
+
+make new change
